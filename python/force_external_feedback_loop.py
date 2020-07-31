f84eef7208606df4a89a8d26d822527a0fbced44
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2020-07-31 16:56:53 +0100

    Testing complete also

--- python/force_external_feedback_loop.py
+++ python/force_external_feedback_loop.py
@@ -48,9 +48,12 @@ class Sine4_1A():
             self.normal_rng.uniform(size=(self.N, 1))
             - 0.5
         )
+
         # weights for the recurrent network
+        # can load a mat file
         #  self.M = scipy.io.loadmat("M.mat")
         #  self.M = self.M['b']
+        # or generate it here
         rvs = scipy.stats.norm(loc=0.0, scale=1.0).rvs
         self.M = scipy.sparse.random(
             self.N, self.N, density=self.p,
@@ -59,14 +62,20 @@ class Sine4_1A():
         ) * self.g * self.scale
         self.M = self.M.toarray()
 
-        print("M is of type {}".format(type(self.M)))
-
         numpy.savetxt("M.csv", self.M, fmt='%.10f',
                       delimiter=',', newline='\n')
 
         # For stimulus signals
         self.amp = 1.3
         self.freq = 1.0/60.
+
+        # initial values
+        self.x0 = (
+            numpy.random.default_rng().standard_normal(size=(self.N, 1))
+            * 0.5)
+        self.z0 = (
+            numpy.random.default_rng().standard_normal(size=(1))
+            * 0.5)
 
         # Print all parameters
         print("{:<30}:{:<10} neurons".format("N", self.N))
@@ -78,85 +87,6 @@ class Sine4_1A():
         print("{:<30}:{:<10} dts".format("learn_every", self.learn_every))
         print("{:<30}:{:<10}".format("scale", self.scale))
         print("Network initialised.")
-
-    def __train(self, traintime=None):
-        """
-        Train the system
-
-        :traintime: length of training time (nsecs by default)
-        :returns: nothing
-
-        """
-        if not traintime:
-            traintime = self.nsecs
-        print("Training network for {} seconds".format(traintime))
-
-        # simulation time: training
-        self.simtime_training = numpy.arange(0, traintime - self.dt, self.dt)
-
-        # training signal
-        self.ft = self.__setup_sin_signals(self.amp, self.freq,
-                                           self.simtime_training)
-
-        # hold values of z during the simulation
-        self.zt = numpy.zeros(shape=(1, len(self.simtime_training)))
-        # hold previous values of z during the simulation
-        self.zpt = numpy.zeros(shape=(1, len(self.simtime_training)))
-        # to hold magnitude of weight vector
-        self.wo_mag = numpy.zeros(shape=(1, len(self.simtime_training)))
-
-        # initial values
-        x0 = numpy.random.default_rng().standard_normal(size=(self.N, 1)) * 0.5
-        z0 = numpy.random.default_rng().standard_normal(size=(1)) * 0.5
-
-        x = x0
-        r = numpy.tanh(x)
-        z = z0
-
-        # progress bar
-        widgets = [progressbar.Percentage(), progressbar.Bar(),
-                   progressbar.FormatLabel(" Time elapsed: %(elapsed)s")]
-        bar = progressbar.ProgressBar(widgets=widgets, max_value=100).start()
-        ti = 0
-        for t in self.simtime_training:
-            # neuronal activity in recurrent network
-            x = (
-                (1.0-self.dt) * x +
-                self.M.dot(r * self.dt) +
-                self.wf * (z * self.dt)
-            )
-            # rates of neurons in the recurrent network
-            r = numpy.tanh(x)
-            # output signal
-            z = numpy.dot(self.wo.transpose(), r)
-
-            if ti % self.learn_every == 0:
-                # update inverse correlation matrix
-                self.P = self.__update_P(self.P, r)
-
-                # Update weights
-                self.wo = self.__update_w(self.wo, z, r, self.P, self.ft[ti])
-
-            self.zt[0][ti] = z
-            self.wo_mag[0][ti] = numpy.sqrt(
-                numpy.dot(self.wo.transpose(), self.wo))
-
-            # increment time
-            ti += 1
-            if ti % 10 == 0:
-                bar.update(ti/len(self.simtime_training) * 100)
-
-        bar.finish()
-        error_avg = ((numpy.abs(self.zt - self.ft).sum())
-                     / len(self.simtime_training))
-        print("Training MAE: {}".format(error_avg))
-
-        numpy.savetxt("ft.csv", self.ft.transpose(), fmt='%.10f',
-                      delimiter='\t', newline='\n')
-        numpy.savetxt("zt.csv", self.zt.transpose(), fmt='%.10f',
-                      delimiter='\t', newline='\n')
-        numpy.savetxt("wo_mag.csv", self.wo_mag.transpose(), fmt='%.10f',
-                      delimiter='\t', newline='\n')
 
     def __update_P(self, P, r):
         """
@@ -195,27 +125,6 @@ class Sine4_1A():
         dw = -e_minus * k * c
         return(wo + dw)
 
-    def __test(self, testtime=None):
-        """
-        Test the system
-
-        :testtime: length of testing phase (nsecs by default)
-
-        :returns: nothing
-
-        """
-        if not testtime:
-            testtime = self.nsecs
-        print("Testing network for {} seconds".format(testtime))
-
-        # simulation time: testing
-        self.simtime_testing = numpy.arange(self.simtime_training,
-                                            2 * self.nsecs - self.dt,
-                                            self.dt)
-        # testing signal
-        self.ft2 = self.__setup_sin_signals(self.amp, self.freq,
-                                            self.simtime_testing)
-
     def __setup_sin_signals(self,  amp, freq, simtime):
         """
         Set up sin signals
@@ -235,16 +144,153 @@ class Sine4_1A():
         ft /= 1.5
         return ft
 
-    def runSim(self):
+    def train(self, traintime=None):
         """
-        Run the simulation
+        Train the system
+
+        :traintime: length of training time (nsecs by default)
+        :returns: nothing
+
+        """
+        if not traintime:
+            traintime = self.nsecs
+
+        # simulation time: training
+        self.simtime_training = numpy.arange(self.dt, traintime + self.dt,
+                                             self.dt)
+        print("Training network for {} seconds in {} steps".format(
+            self.simtime_training[-1] - self.simtime_training[0] + self.dt,
+            len(self.simtime_training)))
+
+        # training signal
+        self.ft = self.__setup_sin_signals(self.amp, self.freq,
+                                           self.simtime_training)
+
+        # hold values of z during the simulation
+        self.z_training = numpy.zeros(shape=(1, len(self.simtime_training)))
+        # to hold magnitude of weight vector
+        self.wo_mag = numpy.zeros(shape=(1, len(self.simtime_training)))
+
+        # initialise
+        self.x = self.x0
+        r = numpy.tanh(self.x)
+        self.z = self.z0
+
+        # progress bar
+        widgets = [progressbar.Percentage(), progressbar.Bar(),
+                   progressbar.FormatLabel(" Time elapsed: %(elapsed)s")]
+        bar = progressbar.ProgressBar(widgets=widgets, max_value=100).start()
+        ti = 0
+        for t in self.simtime_training:
+            # neuronal activity in recurrent network
+            self.x = (
+                (1.0-self.dt) * self.x +
+                self.M.dot(r * self.dt) +
+                self.wf * (self.z * self.dt)
+            )
+            # rates of neurons in the recurrent network
+            r = numpy.tanh(self.x)
+            # output signal
+            self.z = numpy.dot(self.wo.transpose(), r)
+
+            if ti % self.learn_every == 0:
+                # update inverse correlation matrix
+                self.P = self.__update_P(self.P, r)
+
+                # Update weights
+                self.wo = self.__update_w(self.wo, self.z, r,
+                                          self.P, self.ft[ti])
+
+            self.z_training[0][ti] = self.z
+            self.wo_mag[0][ti] = numpy.sqrt(
+                numpy.dot(self.wo.transpose(), self.wo))
+
+            # increment time
+            ti += 1
+            if ti % 10 == 0:
+                bar.update(ti/len(self.simtime_training) * 100)
+
+        bar.finish()
+        error_avg = ((numpy.abs(self.z_training - self.ft).sum())
+                     / len(self.simtime_training))
+        print("Training MAE: {}".format(error_avg))
+
+        numpy.savetxt("ft-train.csv", self.ft.transpose(), fmt='%.10f',
+                      delimiter='\t', newline='\n')
+        numpy.savetxt("zt-train.csv", self.z_training.transpose(), fmt='%.10f',
+                      delimiter='\t', newline='\n')
+        numpy.savetxt("wo_mag-train.csv", self.wo_mag.transpose(), fmt='%.10f',
+                      delimiter='\t', newline='\n')
+
+    def test(self, testtime=None):
+        """
+        Test the system
+
+        :testtime: length of testing phase (nsecs by default)
 
         :returns: nothing
 
         """
-        self.__train()
+        if not testtime:
+            testtime = self.nsecs
+
+        # simulation time: testing
+        self.simtime_testing = numpy.arange(
+            self.simtime_training[-1] + self.dt,
+            2 * self.nsecs + self.dt,
+            self.dt
+        )
+        print("Testing network for {} seconds in {} steps".format(
+            self.simtime_testing[-1] - self.simtime_testing[0] + self.dt,
+            len(self.simtime_testing)))
+
+        # hold values of z during the simulation
+        self.z_testing = numpy.zeros(shape=(1, len(self.simtime_testing)))
+        # testing signal
+        self.ft2 = self.__setup_sin_signals(self.amp, self.freq,
+                                            self.simtime_testing)
+
+        # do not need to be reinitialised
+        #  self.x = self.x0
+        #  self.z = self.z0
+        r = numpy.tanh(self.x)
+
+        # progress bar
+        widgets = [progressbar.Percentage(), progressbar.Bar(),
+                   progressbar.FormatLabel(" Time elapsed: %(elapsed)s")]
+        bar = progressbar.ProgressBar(widgets=widgets, max_value=100).start()
+        ti = 0
+        for t in self.simtime_testing:
+            # neuronal activity in recurrent network
+            self.x = (
+                (1.0-self.dt) * self.x +
+                self.M.dot(r * self.dt) +
+                self.wf * (self.z * self.dt)
+            )
+
+            # rates of neurons in the recurrent network
+            r = numpy.tanh(self.x)
+            # output signal
+            self.z = numpy.dot(self.wo.transpose(), r)
+
+            self.z_testing[0][ti] = self.z
+
+            # increment time
+            ti += 1
+            if ti % 10 == 0:
+                bar.update(ti/len(self.simtime_testing) * 100)
+
+        error_avg = ((numpy.abs(self.z_testing - self.ft2).sum())
+                     / len(self.simtime_testing))
+        print("Testing MAE: {}".format(error_avg))
+
+        numpy.savetxt("ft-test.csv", self.ft2.transpose(), fmt='%.10f',
+                      delimiter='\t', newline='\n')
+        numpy.savetxt("zt-test.csv", self.z_testing.transpose(), fmt='%.10f',
+                      delimiter='\t', newline='\n')
 
 
 if __name__ == "__main__":
     sim = Sine4_1A()
-    sim.runSim()
+    sim.train()
+    sim.test()
